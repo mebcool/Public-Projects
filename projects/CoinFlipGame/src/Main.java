be6bfb58bfa711d9691925d6d9d4b122c616306d
mebcool <160055684+mebcool@users.noreply.github.com>
2024-09-06 11:30:59 -0600

    Delete projects directory

--- projects/CoinFlipGame/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //Group partners: Matt Eddy and Sebastian Pardee
-        //Run server, and then Main
-
-        Controller controller = new Controller();
-    }
-}
